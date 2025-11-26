cc5d993a47db286fdec66e3b882ba91eb2d3536a
OnlyNoco <abidabdullah.cloud@gmail.com>
2025-11-26 20:05:15 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,5 @@
-from pyrogram import Client, filters 
+from pyromod.listen import Client
+from pyrogram import filters 
 import psutil, shutil
 from bot import Bot
 from config import OWNER_ID, START_MSG, START_PHOTO
