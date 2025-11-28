6919d6727c999f8368d55db57d48c4afece16efb
OnlyNoco <abidabdullah.cloud@gmail.com>
2025-11-28 23:17:42 +0600

    Update database.py

--- database/database.py
+++ database/database.py
@@ -3,15 +3,24 @@ from config import DB_URL, DB_NAME
 
 class MongoDB:
   def __init__(self, uri, name):
-    self.db = AsyncIOMotorClient(uri)[name] 
-    self.collection = self.db["settings"] 
-  
-  
+    self.client = AsyncIOMotorClient(uri)
+    self.db = self.client[name]
+    self.collection = self.db["settings"]
+
+  async def init_collection(self):
+    """Ensure the settings doc exists (safe upsert)."""
+    await self.collection.update_one(
+      {"_id": "bot_settings"},
+      {"$setOnInsert": {"_id": "bot_settings"}},
+      upsert=True
+    )
+
   async def get_settings(self):
     data = await self.collection.find_one({"_id": "bot_settings"})
     return data or {}
-  
-  async def update_settings(self, key, value):
+
+  # <-- RENAMED: singular to match Bot.update_setting calls
+  async def update_setting(self, key, value):
     await self.collection.update_one(
       {"_id": "bot_settings"},
       {"$set": {key: value}},
