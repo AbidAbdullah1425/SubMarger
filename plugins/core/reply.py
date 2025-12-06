73e278c8a3686da5a5a847abe1ebcff9f108da73
OnlyNoco <abidabdullah.cloud@gmail.com>
2025-12-06 17:54:30 +0600

    Update reply.py

--- plugins/core/reply.py
+++ plugins/core/reply.py
@@ -1,1 +1,13 @@
 # DIEOAJJSSS
+
+
+
+
+
+
+@Bot.on_message(
+    filters.user(OWNER_ID) &
+    (filters.video | (filters.document & filters.create(lambda _, __, m: m.document and (m.document.file_name.endswith((".ass", ".srt"))))))
+)
+async def handle_reply(client: Client, message: Message):
+    
